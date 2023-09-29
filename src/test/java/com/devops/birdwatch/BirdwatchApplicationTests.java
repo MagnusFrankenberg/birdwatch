85e79a1a6f2b4134eca3b8c49baea975eb650cf0
MagnusFrankenberg <frankenberg.magnus@gmail.com>
2023-09-29 15:55:42 +0200

    turn off test-specific properties

--- src/test/java/com/devops/birdwatch/BirdwatchApplicationTests.java
+++ src/test/java/com/devops/birdwatch/BirdwatchApplicationTests.java
@@ -2,9 +2,8 @@ package com.devops.birdwatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 @SpringBootTest
 class BirdwatchApplicationTests {
 

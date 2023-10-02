37bcc54e3e0a6b8b441a7200b1b8fb836ddf2e38
MagnusFrankenberg <frankenberg.magnus@gmail.com>
2023-10-02 17:19:04 +0200

    application-test.properties deleted

--- src/test/java/com/devops/birdwatch/BirdwatchApplicationTests.java
+++ src/test/java/com/devops/birdwatch/BirdwatchApplicationTests.java
@@ -3,7 +3,7 @@ package com.devops.birdwatch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@ActiveProfiles("test")
+
 @SpringBootTest
 class BirdwatchApplicationTests {
 

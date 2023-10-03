7c36c4ac39a4af789d87ea12a48f81f7a0e0e726
MagnusFrankenberg <frankenberg.magnus@gmail.com>
2023-10-03 17:22:41 +0200

    testing workflow push from feature branch failing tests

---
+++ src/test/java/com/devops/birdwatch/controller/WelcomeControllerTest.java
@@ -0,0 +1,15 @@
+package com.devops.birdwatch.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WelcomeControllerTest {
+
+    WelcomeController welcomeController = new WelcomeController();
+    @Test
+    void welcome() {
+
+        assertEquals("Not Welcome to BirdWatch!",welcomeController.welcome());
+    }
+}

18c351d3557ef1227e2d88f921a6659d7bad0296
MagnusFrankenberg <frankenberg.magnus@gmail.com>
2023-10-04 12:30:04 +0200

    refactor All tests and update workflow 2

--- src/sysTest/java/com.devops.birdwatch/Observation_E2E_Test.java
+++ src/sysTest/java/com.devops.birdwatch/Observation_E2E_Test.java
@@ -2,7 +2,7 @@ package com.devops.birdwatch;
 
 import com.devops.birdwatch.model.Bird;
 import com.devops.birdwatch.model.BirdWatcher;
-import com.devops.birdwatch.model.Observation;
+import com.devops.birdwatch.model.ObservationTemplate;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,66 +15,89 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Observation_E2E_Test {
 
+    Bird savedBird;
+    BirdWatcher savedBirdWatcher;
+
     @BeforeEach
     public void setUpClass() {
         RestAssured.baseURI = "http://localhost:8080";
     }
 
     @Test
-    public void E2E_addObservationAndExpect201(){
+    public void E2E_addObservationAndExpect201() {
 
         //Creating a bird
         Bird bird = new Bird();
         bird.setSpeices("FågelArt1");
         bird.setType("FågelTyp1");
 
-         Bird savedBird = given()
+        savedBird = given()
                 .contentType(ContentType.JSON)
                 .body(bird)
                 .when()
                 .post("/bird/add")
                 .then()
                 .statusCode(201)
-                 .extract().as(Bird.class);
+                .extract().as(Bird.class);
 
 
         //Creating a birdWatcher
         BirdWatcher birdWatcher = new BirdWatcher();
         birdWatcher.setFirstName("Förnamn1");
         birdWatcher.setLastName("Efternamn1");
-        birdWatcher.setEmail("epost@adress1.se");
+        birdWatcher.setEmail("epost@adress2.se");
 
-        BirdWatcher savedBirdWatcher = given()
+        savedBirdWatcher = given()
                 .contentType(ContentType.JSON)
                 .body(birdWatcher)
                 .when()
-                .post("/birdWatcher/add")
+                .post("/birdwatcher/add")
                 .then()
                 .statusCode(201)
                 .extract().as(BirdWatcher.class);
 
 
         //Creating an Observation
-        Observation observation = new Observation();
-        observation.setObservationDate(LocalDate.parse("2022-11-07"));
-        observation.setLocation("Location1");
-        observation.setCountry("Country1");
-        observation.setBird(savedBird);
-        observation.setBirdWatcher(savedBirdWatcher);
+        ObservationTemplate observationTemplate = new ObservationTemplate();
+        observationTemplate.setObservationDate(LocalDate.parse("2022-11-07"));
+        observationTemplate.setLocation("Location1");
+        observationTemplate.setCountry("Country1");
+        observationTemplate.setSpeices("FågelArt1");
+        observationTemplate.setEmail("epost@adress2.se");
 
-         given()
+
+        given()
                 .contentType(ContentType.JSON)
-                .body(observation)
+                .body(observationTemplate)
                 .when()
                 .post("/observation/add")
                 .then()
                 .statusCode(201)
                 .body(equalTo("Successfully added new Observation!"));
-
-
-
-
     }
 
+    /*
+    not working, probably need to delete observation first due to dependencies
+    //Deleting testentries
+    @AfterEach
+    public void cleanUp() {
+        if (savedBird != null) {
+            given()
+                    .contentType(ContentType.JSON)
+                    .when()
+                    .delete("/bird/delete/" + savedBird.getId())
+                    .then()
+                    .statusCode(200);
+        }
 
+        if (savedBirdWatcher != null) {
+            given()
+                    .contentType(ContentType.JSON)
+                    .when()
+                    .delete("/birdwatcher/delete/" + savedBirdWatcher.getId())
+                    .then()
+                    .statusCode(200);
+        }
+
+    }*/
 }
